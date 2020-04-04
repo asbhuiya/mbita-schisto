17cff281b97f65099f4cb1fcfe485f158cb8e968
Ben Arnold <bfarnold@gmail.com>
2020-04-04 16:35:49 -0700

    Add more color palettes

--- R/mbita-schisto-Config.R
+++ R/mbita-schisto-Config.R
@@ -15,6 +15,7 @@
 library(tidyverse)
 library(scales)
 library(RColorBrewer)
+library(viridisLite)
 library(ellipse)
 library(grid)
 library(gridExtra)
@@ -54,6 +55,9 @@ cyellow <- "#FFEE33"
 cgrey <- "#777777"
 
 # very safe color blind pallette
+# http://jfly.iam.u-tokyo.ac.jp/color/
+# http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+# Reference: Bang Wong, Nature Methods 2011: https://www.nature.com/articles/nmeth.1618
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # custom 16 level color palette for prevalence x uncertainty
@@ -63,3 +67,9 @@ qcolsd <- matrix(c(243,238,240, 194,207,220, 161,187,210, 119,161,199,
                    211,101,115, 162,102,125, 133,100,135,  95, 91,136),ncol=16,nrow=3)
 qcols <- apply(qcolsd,2,function(x) rgb(x[1]/255,x[2]/255,x[3]/255))
 
+# NY Times rainbow from the Upshot
+# https://www.nytimes.com/interactive/2020/03/21/upshot/coronavirus-deaths-by-country.html
+nytpal <- c("#510000", "#AC112D", "#EC6D47", "#F2A058", "#F7D269", "#839772", "#325D8A")
+
+# Viridis color palette
+vircols <- viridis(9,begin=0,end=0.97)
